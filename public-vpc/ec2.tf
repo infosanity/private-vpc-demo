097a20b87436c89271aab037e291b88acf4a7377
Andrew Waite <andrew.waite@infosanity.co.uk>
2021-04-02 17:07:51 +0100

    SessionManager Demo via Nat Gateway working

--- public-vpc/ec2.tf
+++ public-vpc/ec2.tf
@@ -6,15 +6,14 @@ resource "aws_instance" "public_noNAT_instance" {
     network_interface_id = aws_network_interface.public_nic.id
     device_index         = 0
   }
-
   key_name = "HP-default"
-
   tags = merge(
     var.tags,
     {
-      Name = "publicInstance"
+      Name = "publicInstance_viaEIP"
     }
   )
+  user_data = "ping 8.8.8.8 -c 10"
 }
 
 resource "aws_network_interface" "public_nic" {
@@ -38,6 +37,12 @@ resource "aws_security_group" "instance_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["172.20.0.0/20"]
+  }
   tags = merge(
     var.tags,
     {
@@ -45,6 +50,7 @@ resource "aws_security_group" "instance_sg" {
     }
   )
 }
+
 resource "aws_eip" "instance_eip" {
   vpc      = true
   instance = aws_instance.public_noNAT_instance.id
@@ -54,4 +60,36 @@ resource "aws_eip" "instance_eip" {
       Name = "publicInstanceEIP"
     }
   )
+}
+
+###############################################################
+# Via NAT Gateway
+resource "aws_instance" "public_NAT_instance" {
+  ami                  = "ami-0d712b3e6e1f798ef" # Amazon2 - eu-west-1
+  instance_type        = "t3.micro"
+  iam_instance_profile = aws_iam_instance_profile.ssm_managed_profile.name
+  key_name             = "HP-default"
+  network_interface {
+    network_interface_id = aws_network_interface.nat_nic.id
+    device_index         = 0
+  }
+  tags = merge(
+    var.tags,
+    {
+      Name = "publicInstance_viaNAT"
+    }
+  )
+  user_data = "ping 8.8.8.8 -c 10"
+}
+
+resource "aws_network_interface" "nat_nic" {
+  subnet_id       = aws_subnet.public_viaNAT.id
+  security_groups = [aws_security_group.instance_sg.id]
+  tags = merge(
+    var.tags,
+    {
+      Name = "publicInstance_NATNIC"
+    }
+  )
+
 }
--- public-vpc/network.tf
+++ public-vpc/network.tf
@@ -1,8 +1,5 @@
 resource "aws_vpc" "public" {
-  cidr_block = "172.20.16.0/20"
-  #   tags = {
-  #     Name = "Public-Demo"
-  #   }
+  cidr_block           = "172.20.16.0/20"
   enable_dns_hostnames = true
   enable_dns_support   = true
   tags = merge(
@@ -62,4 +59,36 @@ resource "aws_route_table" "routes_public" {
 resource "aws_route_table_association" "routes_public_subnet" {
   subnet_id      = aws_subnet.public_noNAT.id
   route_table_id = aws_route_table.routes_public.id
+}
+
+resource "aws_eip" "NAT_GW_EIP" {
+  depends_on = [
+    aws_internet_gateway.igw
+  ]
+
+}
+
+# NAT GW lives inside the public subnet
+resource "aws_nat_gateway" "NAT_GW" {
+  allocation_id = aws_eip.NAT_GW_EIP.id
+  subnet_id     = aws_subnet.public_noNAT.id
+  tags = merge(
+    var.tags,
+    {
+      "Name" = "SessionManager_NatGateway"
+    }
+  )
+}
+
+resource "aws_route_table" "routes_nat" {
+  vpc_id = aws_vpc.public.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.NAT_GW.id
+  }
+}
+
+resource "aws_route_table_association" "routes_via_NATGW" {
+  subnet_id      = aws_subnet.public_viaNAT.id
+  route_table_id = aws_route_table.routes_nat.id
 }
